4ed1d9b53b388c46f22fca4166836fa6bc57dd5a
Ingridlindor <ingridsantiagolindor@gmail.com>
2025-02-21 11:00:39 -0300

    Update Ex003.py

--- Exercicios/Ex003.py
+++ Exercicios/Ex003.py
@@ -1,6 +1,5 @@
-#Desafio 2 - Faça um programa que leia algo e mostre o seu tipo primitivo e todas as informaçõe possiveis sobre ele
-x=(input('Digite algo: '))
-print('O valor é alfa numerico? ',(x.isalnum()))
-print('O valor é um numero? ', (x.isnumeric()))
-print('O valor é um texto? ',(x.isalpha()))
-print (' O valor é decimal  ?', (x.isdecimal()))
+# Script q retorne '' A soma entre n1 e n2 é igual a Resultado''
+n1=(int(input('Digite o primeiro numero: ')))
+n2=(int(input('Digite o segundo numero: ')))
+s=n1+n2
+print('A soma entre {} e {} é igal a {}'.format(n1,n2,s))
