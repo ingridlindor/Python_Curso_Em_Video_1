283481641796b094f1cf9ffda8cf6079878ce8b6
Ingridlindor <ingridsantiagolindor@gmail.com>
2025-02-24 10:45:00 -0300

    Create Ex005.py

---
+++ Exercicios/Ex005.py
@@ -0,0 +1,5 @@
+# Faça um progama que leia um numero inteiro e mostre na tela seu sucessor e seu antecessor 
+n = int(input('Digite um numero: '))
+print('O antecessor do numero {} é {} e o sucessor é {}'.format(n,n-1,n+1))
+
+
