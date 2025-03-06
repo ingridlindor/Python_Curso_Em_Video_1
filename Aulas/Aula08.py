d70a66878204bc8ac31a34b71a2cea9b064b05c3
Ingridlindor <ingridsantiagolindor@gmail.com>
2025-03-06 16:33:52 -0300

    Atualiza Exercicios

---
+++ Aulas/Aula08.py
@@ -0,0 +1,14 @@
+# Utilizando Modulos
+# from math import sqrt
+# n=int(input('Digite um numero: '))
+# raiz = sqrt(n)
+
+# print(' A raiz de {} é igual a {:.0f} '.format(n,raiz))
+
+#Gera numero aleatorio de 0 a 1
+# import random
+# num=random.random()
+# print(num)
+import(emoji)
+print(emoji.emojize(':sunglasses:'))
+
