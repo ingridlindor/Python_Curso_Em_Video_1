d41fa8f4cb34a61b345e3f5fb313f2879c358543
Ingridlindor <ingridsantiagolindor@gmail.com>
2025-03-24 13:43:28 -0300

    Update Ex017.py

--- Exercicios/Ex017.py
+++ Exercicios/Ex017.py
@@ -1,3 +1,7 @@
 #17
 #Faça um programa que leia o comprimento do cateto oposto e do adjacente de um triangulo retangulo e mostre a hipotenusa
+oposto=float(input('Comprimento do cateto oposto: '))
+adjacente=float(input('Comprimento do cateto adjacente: '))
 
+h=
+Print()
